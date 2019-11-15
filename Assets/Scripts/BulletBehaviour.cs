d8779f3319e25b204d83fe5f9b3377f4cf70af46
Robin Jesson <rjesson@utbm.fr>
2019-11-15 17:22:10 +0100

    1er commit

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private float x;
+    private float y;
+    private float z;
+    private DateTime creationTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 target = Camera.main.transform.position;
+        x = target.x;
+        y = target.y;
+        z = target.z;
+        creationTime = DateTime.Now;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = new Vector3(x, y, z);
+        float step = speed * Time.deltaTime;
+        this.transform.position = Vector3.MoveTowards(transform.position, target, step);
+
+        if(DateTime.Now >= creationTime.AddSeconds(20))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ShootBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBehaviour : MonoBehaviour
+{
+    private DateTime lastOccurence;
+
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastOccurence = DateTime.Now;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DateTime.Now >= lastOccurence.AddSeconds(3))
+        {
+            lastOccurence = DateTime.Now;
+            Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
+        }
+    }
+}

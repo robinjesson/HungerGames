478fcc25bb29e508d2b2b9289d88ea704d4d2093
Robin Jesson <rjesson@utbm.fr>
2019-12-13 18:58:16 +0100

    ajout viseur/vies

---
+++ Assets/Scripts/UiFallowCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiFallowCam : MonoBehaviour
+{
+
+    private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(cam.transform.position.x, this.cam.transform.position.y, this.cam.transform.position.z+1);
+        this.transform.rotation = this.cam.transform.rotation;
+    }
+}
--- Assets/Scripts/UiScript.cs
+++ Assets/Scripts/UiScript.cs
@@ -8,7 +8,6 @@ public class UiScript : MonoBehaviour
     public static GameObject life_bd;
     public static GameObject life_hg;
     public static GameObject life_hd;
-    public static GameObject life_text;
     public static int life;
 
     // Start is called before the first frame update
@@ -18,7 +17,6 @@ public class UiScript : MonoBehaviour
         life_bg = GameObject.Find("Vie_bg");
         life_hd = GameObject.Find("Vie_hd");
         life_hg = GameObject.Find("Vie_hg");
-        life_text = GameObject.Find("Life_text");
         life = 4;
     }
 
@@ -34,25 +32,21 @@ public class UiScript : MonoBehaviour
         if(life == 3)
         {
             Destroy(life_bd);
-            life_text.GetComponent<UnityEngine.UI.Text>().text = "3";
             Debug.Log("test1");
         } 
         else if(life == 2)
         {
             Destroy(life_bg);
-            life_text.GetComponent<UnityEngine.UI.Text>().text = "2";
             Debug.Log("test2");
         }
         else if (life == 1)
         {
             Destroy(life_hg);
-            life_text.GetComponent<UnityEngine.UI.Text>().text = "1";
             Debug.Log("test3");
         }
         else if (life == 0)
         {
             Destroy(life_hd);
-            life_text.GetComponent<UnityEngine.UI.Text>().text = "0";
             Debug.Log("test4");
         }
         else
